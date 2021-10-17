28e50a1a68c0792c743e0715d8969b66d4fadee8
nbba-dev <nbba.dev@gmail.com>
2021-10-17 21:23:20 +0200

    add webcomponent to handle login

---
+++ shared/nbba-login.js
@@ -0,0 +1,78 @@
+fetch("/shared/nbba-login.html")
+.then(stream => stream.text())
+.then(text => define(text));
+
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '673474108161-nr43igs2gi8j9nbu6pjnuuc620j45ana.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyCJYgfijMbwroLQX1qBd51rx9UHDgnVrh8';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+function define(html) {
+  class NbbaLogin extends HTMLElement {
+    constructor() {
+      super();
+
+      var shadow = this.attachShadow({mode: 'open'});
+      shadow.innerHTML = html;
+
+      this.loginButton = shadow.querySelector('#nbba-login-btn');
+      this.logoutButton = shadow.querySelector('#nbba-logout-btn');
+      this.text = shadow.querySelector('#nbba-login-status');
+
+      var script = document.createElement('script');
+      script.setAttribute('async', true)
+      script.setAttribute('defer', true)
+      script.onload = () => { gapi.load('client:auth2', () => { this.initClient() }); }
+      script.onreadystatechange = () => { debugger; if (this.readyState === 'complete') this.onload() }
+      script.src = "https://apis.google.com/js/api.js";
+      document.getElementsByTagName('head')[0].appendChild(script);
+    }
+
+    login() {
+      gapi.auth2.getAuthInstance().signIn();
+    }
+
+    logout() {
+      gapi.auth2.getAuthInstance().signOut();
+    }
+
+    initClient() {
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+      }).then(() => {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen((a) => { this.updateSigninStatus(a) });
+
+        // Handle the initial sign-in state.
+        this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+
+        this.loginButton.addEventListener('click', this.login)
+        this.logoutButton.addEventListener('click', this.logout)
+      }, function(error) {
+        alert(JSON.stringify(error, null, 2))
+        // appendPre(JSON.stringify(error, null, 2));
+      });
+    }
+
+    updateSigninStatus(isSignedIn) {
+      if (isSignedIn) {
+        this.loginButton.setAttribute('hidden', true)
+        this.logoutButton.removeAttribute('hidden')
+      } else {
+        this.loginButton.removeAttribute('hidden')
+        this.logoutButton.setAttribute('hidden', true)
+      }
+    }
+  }
+
+  customElements.define('nbba-login', NbbaLogin);
+}
