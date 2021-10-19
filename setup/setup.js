8a44c6ecc79377321ac7fd14881dad3780a1a228
nbba-dev <nbba.dev@gmail.com>
2021-10-19 14:08:41 +0200

    updated manifest

--- setup/setup.js
+++ setup/setup.js
@@ -2,7 +2,8 @@ import loadedGapiPromise from '/shared/nbba-login.js'
 
 loadedGapiPromise.then(() => {
   gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: '1VjjpG46Z36oMZPSlx2bipgiu3pCvB1F-ZsRh4qpJ7JE',
+    spreadsheetId: '1DMWedcFO_9MvNAOhd4mTzxzhKaNsTgoSaaR_Ua5F42Q',
+    // spreadsheetId: '1VjjpG46Z36oMZPSlx2bipgiu3pCvB1F-ZsRh4qpJ7JE',
     range: 'A2:E',
   }).then(function(response) {
     var range = response.result;
