fe453cd836a3bd8b863d8008fdedf43af3bc53b2
nbba-dev <nbba.dev@gmail.com>
2022-01-25 18:27:26 +0100

    added home screen mock

---
+++ home/home.js
@@ -0,0 +1,7 @@
+document.querySelector('#playGame').addEventListener('click', () => {
+  window.open('/gameSetup', '_self')
+})
+
+document.querySelector('#playLeagueGame').addEventListener('click', () => {
+  window.open('/leagueGameSetup', '_self')
+})
